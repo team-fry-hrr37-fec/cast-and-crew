8352319064818dd6707c977ed7a37a916ae89ac5
Danielxburns <danielburnsart@gmail.com>
2019-04-01 18:10:15 -0500

    setup barebones front end

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dbIndex = require('../database/index.js');
+const port = 2002;
+
+let database = 'fandangit';
+mongoose.connect(`mongodb://localhost/${database}`, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log(`connected to ${database}!`);
+});
+
+app.use('/', express.static(__dirname + '/../client/dist')); // verify this path when front end is up
+
+app.use(express.json());
+
+app.get('/actors', ((req, res) => {
+  console.log('inside GET');
+  dbIndex.find(actors => {
+    console.log(`GET response to '/'=${actors}`);
+    res.send(actors);
+  });
+}));
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}!`);
+});
+
