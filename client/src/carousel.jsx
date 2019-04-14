94105289785cba546ddc9a40d744a9cf0e627111
Danielxburns <danielburnsart@gmail.com>
2019-04-13 19:35:15 -0500

    deletes some comments

--- client/src/carousel.jsx
+++ client/src/carousel.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-// import styled from 'styled-components';
 import CarouselItem from './carouselItem.jsx';
 import Popup from 'reactjs-popup';
-
 
 // === STYLES === //
 
